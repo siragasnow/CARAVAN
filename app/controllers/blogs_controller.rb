a75950e0665802c06fef4b9d2c919ecc5ecb60a9
siragasnow <siraga613@gmail.com>
2020-05-30 10:32:37 +0900

    [start]4-2

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,16 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def editnew
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/editnew'
+  get 'blogs/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
