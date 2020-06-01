26e93d216de35e3f0fc0636bf0bad5a389d63f2b
siragasnow <siraga613@gmail.com>
2020-06-01 15:37:17 +0900

    [start]4-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def index
@@ -13,7 +14,7 @@ class BlogsController < ApplicationController
   def create
   	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blogs_path
+  	redirect_to blog_path
   end
 
   def edit
