ace2d160b0e461e182fbd06a6e495ad1a0001686
siragasnow <siraga613@gmail.com>
2020-06-01 16:03:30 +0900

    [finish]4-5

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,7 +14,7 @@ class BlogsController < ApplicationController
   def create
   	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blog_path
+  	redirect_to blog_path(blog.id)
   end
 
   def edit
