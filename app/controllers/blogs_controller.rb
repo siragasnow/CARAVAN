d368a62e4fd8b7a747cea3cb43a32a45ba45e299
siragasnow <siraga613@gmail.com>
2020-06-01 16:30:54 +0900

    [finish]4-6

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,21 +8,27 @@ class BlogsController < ApplicationController
   end
 
   def new
-  	@blog = Blog.new
+    @blog = Blog.new
   end
 
   def create
-  	blog = Blog.new(blog_params)
-  	blog.save
-  	redirect_to blog_path(blog.id)
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blog_path(blog.id)
   end
 
   def edit
     @blog = Blog.find(params[:id])
   end
 
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
   private
   def blog_params
-  	params.require(:blog).permit(:title, :category, :body)
+    params.require(:blog).permit(:title, :category, :body)
   end
 end
