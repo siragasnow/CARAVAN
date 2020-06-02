2bbf32e74872be749a4f5253346e65ea3a918cd1
siragasnow <siraga613@gmail.com>
2020-06-02 11:22:08 +0900

    [start]4-7

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -27,6 +27,12 @@ class BlogsController < ApplicationController
     redirect_to blog_path(blog)
   end
 
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
+  end
+
   private
   def blog_params
     params.require(:blog).permit(:title, :category, :body)
